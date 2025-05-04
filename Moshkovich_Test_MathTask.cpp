e80d85174dfa05b15e3ac2e3e8bae95243bb873b
Alinrrrr <amberhhs4@@gmail.com>
2025-05-04 21:57:39 +0500

    changes

--- Moshkovich_Test_MathTask.cpp
+++ Moshkovich_Test_MathTask.cpp
@@ -1,6 +1,6 @@
 #include "CUTE/cute/cute.h"
 #include "CUTE/cute/ide_listener.h"
-#include "CUTE/cute/cute_runner.h""
+#include "CUTE/cute/cute_runner.h"
 //подключение модуля с тестируемыми методами
 #include "Moshkovich_MathTask.h"
 
@@ -18,6 +18,50 @@ void testCalcRectangleArea() {
     //сравнение ожидаемого результата с полученным
     ASSERT_EQUAL(expected,actual);
 }
+    //тест проверяющий отсев пустых значений
+void testUserUnput_Empty(){
+//исходные данные
+string str = "";
+//
+bool expected = false;
+//
+bool actual = UserInput(str);
+//
+ASSERT_EQUAL(expected,actual);
+}
+//тест проверяющий отсев нецифровых  символов
+void testUserUnput_Letter(){
+//исходные данные
+string str = "a";
+//
+bool expected = false;
+//
+bool actual = UserInput(str);
+//
+ASSERT_EQUAL(expected,actual);
+}
+//тест проверяющий отсев цифро-буквенных значений
+void testUserUnput_DigitLetterValue(){
+//исходные данные
+string str = "5a";
+//
+bool expected = false;
+//
+bool actual = UserInput(str);
+//
+ASSERT_EQUAL(expected,actual);
+}
+//тест проверяющий отсев отрицательных значений
+void testUserUnput_NegativeValue(){
+//исходные данные
+string str = "-5";
+//
+bool expected = false;
+//
+bool actual = UserInput(str);
+//
+ASSERT_EQUAL(expected,actual);
+}
 
 int main() {
     /**/ //создаем тестовый набор
@@ -25,6 +69,10 @@ int main() {
 
     //Добавляем тестовую функцию в набор
     s.push_back(CUTE(testCalcRectangleArea));
+    s.push_back(CUTE(testUserUnput_Empty));
+    s.push_back(CUTE(testUserUnput_Letter));
+    s.push_back(CUTE(testUserUnput_DigitLetterValue));
+    s.push_back(CUTE(testUserUnput_NegativeValue));
   
     //созддаем listener и Runner
     ide_listener<> listener;
